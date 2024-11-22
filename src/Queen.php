a450ba2b6e6ab28489e4c2ee3110992d78ae4f99
Akhmetov Ruslan <raslhaslproduced@gmail.com>
2024-11-22 09:09:34 +0500

    Изменил Ферзя

--- src/Queen.php
+++ src/Queen.php
@@ -3,6 +3,36 @@
 require_once('IFigure.php');
 
 class Queen extends Figure {
- 
     protected array $icon = ["\u{265B}", "\u{2655}"];
+    public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+       
+        if ($from_row === $to_row || $from_col === $to_col) {
+            return $this->isPathClear($from_row, $from_col, $to_row, $to_col, $board);
+        }
+
+        if (abs($from_row - $to_row) === abs($from_col - $to_col)) {
+            return $this->isPathClear($from_row, $from_col, $to_row, $to_col, $board);
+        }
+
+        return false;
+    }
+
+    private function isPathClear(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+
+        $rowStep = $from_row === $to_row ? 0 : ($to_row > $from_row ? 1 : -1);
+        $colStep = $from_col === $to_col ? 0 : ($to_col > $from_col ? 1 : -1);
+
+        $currentRow = $from_row + $rowStep;
+        $currentCol = $from_col + $colStep;
+
+        while ($currentRow !== $to_row || $currentCol !== $to_col) {
+            if ($board->getItem($currentRow, $currentCol) !== null) {
+                return false; 
+            }
+            $currentRow += $rowStep;
+            $currentCol += $colStep;
+        }
+
+        return true; 
+}
 }
